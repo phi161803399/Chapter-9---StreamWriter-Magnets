51b515e21f34c8b92be25b05f6ec990d450b186b
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-06-08 11:54:41 +0200

    Add project files.

---
+++ Chapter 9 - StreamWriter Magnets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_9___StreamWriter_Magnets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
